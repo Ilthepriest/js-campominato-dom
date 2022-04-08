7a7858574cc1ddcfe2d1f0ca26a3a60d0d7ec622
Ilthepriest <robertodelprete1993@libero.it>
2022-04-08 16:39:19 +0200

    not work removeEventListener

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-function creoGriglia(valore){
+function creoGriglia(valore) {
     const celle = document.querySelector(".container");
     celle.innerHTML = "";
-    
+
 
 
     for (let i = 1; i <= valore; i++) {
@@ -10,33 +10,34 @@ function creoGriglia(valore){
         cella.classList.add("cella");
         cella.innerHTML = `${i}`
         celle.append(cella)
-    
+        // console.log(celle.textContent);
     }
-    
+
+    return celle
 }
 
 
 const bottone = document.querySelector("button")
 
-bottone.addEventListener('click', function(){
+bottone.addEventListener('click', function () {
     const lista = document.querySelector("select").value;
     console.log(lista)
 
-    if(lista === "easy"){
+    if (lista === "easy") {
         creoGriglia(100)
-        let listaNumeriRandom = generateCellsNumbers(100)
+        const listaNumeriRandom = generateCellsNumbers(100)
         handleClick(listaNumeriRandom)
+
+
+    } else if (lista === "medium") {
         
-        
-    }else if(lista === "medium"){
-        creoGriglia(81)
-        let listaNumeriRandom = generateCellsNumbers(81)
+        const listaNumeriRandom = generateCellsNumbers(81)
         handleClick(listaNumeriRandom)
-        
-        
-    }else if(lista === "hard"){
+
+
+    } else if (lista === "hard") {
         creoGriglia(49)
-        let listaNumeriRandom = generateCellsNumbers(49)
+        const listaNumeriRandom = generateCellsNumbers(49)
         handleClick(listaNumeriRandom)
     }
 });
@@ -50,81 +51,68 @@ bottone.addEventListener('click', function(){
  * @param {number} max Max number
  * @returns {number}
  */
- function getRandomInteger(min, max) {
+function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+}
+
 
 // Generare 16 numeri casuali unici ordinati
 
-  function generateCellsNumbers(num) {
+function generateCellsNumbers(num) {
     const randomNumbers = [];
     while (randomNumbers.length !== 16) {
-      const randomNumber = getRandomInteger(1, num)
-      
-      if (!randomNumbers.includes(randomNumber)) {
-        randomNumbers.push(randomNumber)
-      }
+        const randomNumber = getRandomInteger(1, num)
+
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber)
+        }
     }
     return randomNumbers.sort((a, b) => a - b);
-  }
+}
 
 // console.log(generateCellsNumbers());
 
 
-function handleClick(dato){
+function handleClick(dato, valore) {
     
-    console.log(dato);
-    // 1. selezionare tutte le celle querySelectorAll
+    let numeriCasuali = dato
+   
+    // console.log(tabellaNumeri);
+    console.log({ numeriCasuali });
     const cells = document.querySelectorAll(".cella")
     let lista_numeri_selezioanti = [];
-    
-    //console.log(cells);
-    // 2. ciclare tra gli elementi della dom
+
     for (let i = 0; i < cells.length; i++) {
-      const cellElemnt = cells[i];
-      const numeri = Number(cellElemnt.textContent)
-      
-      
-    //   console.log(cellElemnt);
-      // 3. attacchare l'event listener all'elemento della dom (cell)
-      cellElemnt.addEventListener('click', function() {
-        // console.log(numeri);
-        lista_numeri_selezioanti.push(numeri);
-
-              
-        // 4. evidenziare la cella di azzurro. 
-        //this.style.backgroundColor = 'cornflowerblue'
-          if(dato.includes(numeri)) {
-              cellElemnt.classList.add("red")
-              alert("HAI PERSO")
-              setTimeout(tempo, 600);
-              function tempo(){
-                window.location.reload()
-              }
-              
-        }else if(!dato.includes(numeri)){
-              cellElemnt.classList.add("azzurro")
-             
+        const cellElemnt = cells[i];
+        let numero = Number(cellElemnt.textContent)
+        
+        cellElemnt.addEventListener('click', clickcell) 
+        
+        function clickcell() {
+            lista_numeri_selezioanti.push(numero)
+            console.log(numero);
+            if (dato.includes(numero)) {
+                cellElemnt.classList.add("red")
+                alert("HAI PERSO")
+                cellElemnt.removeEventListener('click', clickcell)
+               
+            }else if (!dato.includes(numero)) {
+                cellElemnt.classList.add("azzurro")
+               
+            } else if (lista_numeri_selezioanti.length === 84 && cells.length === 100 || lista_numeri_selezioanti.length === 65 && cells.length === 81 || lista_numeri_selezioanti.length == 2 && cells.length == 49) {
+                alert(`HAI VINTO hai clickato correttamente ${lista_numeri_selezioanti.length}`)
+            } 
         }
         
-        if(lista_numeri_selezioanti.length === 84 && cells.length === 84 || lista_numeri_selezioanti.length === 65  && cells.length === 65 || lista_numeri_selezioanti.length === 33 && cells.length === 49){
-            alert(`HAI VINTO hai clickato correttamente ${lista_numeri_selezioanti.length}`)
-            setTimeout(tempo, 600);
-              function tempo(){
-                window.location.reload()
-              }
-        }
-        });
-        
-    }
-    
-  }
+    }   
+}
 
 
 
 
-  
+
+
+
 
 
 
