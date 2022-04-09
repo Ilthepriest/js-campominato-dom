208f6f2507d59b0e63bd06565cc40a374cc748f8
Ilthepriest <robertodelprete1993@libero.it>
2022-04-09 23:49:19 +0200

    add bonus 1 and bonus 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Funzione per creare la griglia 
 
 function creoGriglia(valore) {
     const celle = document.querySelector(".container");
@@ -17,23 +18,24 @@ function creoGriglia(valore) {
 }
 
 
+//Evento click del bottone play e condizionale in base alla difficoltà 
+
 const bottone = document.querySelector("button")
 
 bottone.addEventListener('click', function () {
     const lista = document.querySelector("select").value;
     console.log(lista)
 
+    lista_numeri_selezioanti = [];
     if (lista === "easy") {
         creoGriglia(100)
         const listaNumeriRandom = generateCellsNumbers(100)
         handleClick(listaNumeriRandom)
 
-
     } else if (lista === "medium") {
-        
+        creoGriglia(81)
         const listaNumeriRandom = generateCellsNumbers(81)
         handleClick(listaNumeriRandom)
-
 
     } else if (lista === "hard") {
         creoGriglia(49)
@@ -43,7 +45,7 @@ bottone.addEventListener('click', function () {
 });
 
 
-//Genera numero random
+//Funzione genera numero random
 
 /**
  * 
@@ -60,7 +62,7 @@ function getRandomInteger(min, max) {
 
 function generateCellsNumbers(num) {
     const randomNumbers = [];
-    while (randomNumbers.length !== 16) {
+    while (randomNumbers.length != 16) {
         const randomNumber = getRandomInteger(1, num)
 
         if (!randomNumbers.includes(randomNumber)) {
@@ -70,42 +72,74 @@ function generateCellsNumbers(num) {
     return randomNumbers.sort((a, b) => a - b);
 }
 
-// console.log(generateCellsNumbers());
 
+let numeriCasuali;   //<---- variabile globale
 
-function handleClick(dato, valore) {
+//Funzione ciclo ogni cella per mettere evento click ad ognuna di essa
+
+function handleClick(dato) {
     
-    let numeriCasuali = dato
-   
-    // console.log(tabellaNumeri);
+    numeriCasuali = dato
     console.log({ numeriCasuali });
     const cells = document.querySelectorAll(".cella")
-    let lista_numeri_selezioanti = [];
+    
+    for (let i = 0; i < cells.length; i++) {
+        const cellElemnt = cells[i];    
+        cellElemnt.addEventListener('click', clickcell);  
+    }
+}
 
+// Funzione condizionale aggiungo o rimuovo la classe azzurra o red delle celle
+
+let lista_numeri_selezioanti = [];   //<---- variabile globale
+
+function clickcell() {
+    const cells = document.querySelectorAll(".cella")
+    lista_numeri_selezioanti.push(this.innerHTML);
+    
+    if (!numeriCasuali.includes(Number(this.innerHTML))) {
+        this.classList.add("azzurro")
+        
+    } else {
+        this.classList.add("red")
+        alert("HAI PERSO")
+        const cells = document.querySelectorAll(".cella")
+        this.removeEventListener('click', clickcell)
+        endGame()
+        mostroBombe()
+          setTimeout(function(){
+              window.location.reload()
+          },4000)
+    } 
+
+    // se lunghezza lista_numeri_selezioanti ==  cells.length - numeriCasuali.length
+
+    if (lista_numeri_selezioanti.length == cells.length - numeriCasuali.length) {
+        alert(`HAI VINTO hai clickato correttamente ${lista_numeri_selezioanti.length} celle`)
+    } 
+}
+
+//Rimuovo a tutte le caselle evento click ciclando e prendendo ogni casella tramite indice
+
+function endGame(){
+    const cells = document.querySelectorAll(".cella");
     for (let i = 0; i < cells.length; i++) {
         const cellElemnt = cells[i];
-        let numero = Number(cellElemnt.textContent)
-        
-        cellElemnt.addEventListener('click', clickcell) 
-        
-        function clickcell() {
-            lista_numeri_selezioanti.push(numero)
-            console.log(numero);
-            if (dato.includes(numero)) {
-                cellElemnt.classList.add("red")
-                alert("HAI PERSO")
-                cellElemnt.removeEventListener('click', clickcell)
-               
-            }else if (!dato.includes(numero)) {
-                cellElemnt.classList.add("azzurro")
-               
-            } else if (lista_numeri_selezioanti.length === 84 && cells.length === 100 || lista_numeri_selezioanti.length === 65 && cells.length === 81 || lista_numeri_selezioanti.length == 2 && cells.length == 49) {
-                alert(`HAI VINTO hai clickato correttamente ${lista_numeri_selezioanti.length}`)
-            } 
-        }
-        
-    }   
+        cellElemnt.removeEventListener('click', clickcell);
+    }
 }
+
+//Se l'utente perde mostro le posizioni di tutte le bombe
+function mostroBombe(){
+    const cells = document.querySelectorAll(".cella");
+    for (let i = 0; i < cells.length; i++) {
+        const cellElemnt = cells[i];
+        if(numeriCasuali.includes(Number(cellElemnt.innerHTML))) {
+            cellElemnt.classList.add("red")
+        }
+    }
+}
+
 
 
 
